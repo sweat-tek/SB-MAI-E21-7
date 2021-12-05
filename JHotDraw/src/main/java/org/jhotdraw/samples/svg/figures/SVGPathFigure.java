3cb8825a12630b2f8a51917bbd47c1b290460f99
Hejoodatnab <32075278+Hejoodatnab@users.noreply.github.com>
2021-12-05 19:06:40 +0100

    Added Handler for Duplicate Code
    Added a handler class for Duplicate Code in SVGPathFigure and SVGAttributedFigure for duplicate code which is then used for both classes. Both of classes are in the ..samples.svg.figures package

--- JHotDraw/src/main/java/org/jhotdraw/samples/svg/figures/SVGPathFigure.java
+++ JHotDraw/src/main/java/org/jhotdraw/samples/svg/figures/SVGPathFigure.java
@@ -95,17 +95,10 @@ public class SVGPathFigure extends AbstractAttributedCompositeFigure implements 
                             Math.max(1, (int) ((2 + drawingArea.width) * g.getTransform().getScaleX())),
                             Math.max(1, (int) ((2 + drawingArea.height) * g.getTransform().getScaleY())),
                             BufferedImage.TYPE_INT_ARGB);
-                    Graphics2D gr = buf.createGraphics();
-                    gr.scale(g.getTransform().getScaleX(), g.getTransform().getScaleY());
-                    gr.translate((int) -drawingArea.x, (int) -drawingArea.y);
-                    gr.setRenderingHints(g.getRenderingHints());
+                    DrawingAreaHandler handler = new DrawingAreaHandler();
+                    Graphics2D gr = handler.drawingAreaCreater(g, opacity, drawingArea, buf);
                     drawFigure(gr);
-                    gr.dispose();
-                    Composite savedComposite = g.getComposite();
-                    g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, (float) opacity));
-                    g.drawImage(buf, (int) drawingArea.x, (int) drawingArea.y,
-                            2 + (int) drawingArea.width, 2 + (int) drawingArea.height, null);
-                    g.setComposite(savedComposite);
+                    handler.drawingAreaDispose(g, opacity, drawingArea, gr, buf);
     }
     
     public void drawFigure(Graphics2D g) {
